38f4c2633774d540175c93b64ca041024276386b
PJ <spandana.pj@accenture.com>
2023-06-19 11:34:22 +0530

    modified settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -133,9 +133,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # sending emails
 
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='spandanapj1309@gmail.com'
-EMAIL_HOST_PASSWORD='googlelover'
+EMAIL_HOST=''
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
